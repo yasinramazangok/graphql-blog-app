bdd8bc93ef211f47ebed6a5a35eab77d68957374
yasinramazangok <yasinrmzngok@gmail.com>
2025-07-24 14:15:43 +0300

    feat(client): initialize React app and configure files

---
+++ client/src/components/ArticleList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ArticleList(){
+    return(
+        <di>
+            Makale Listesi
+        </di>
+    )
+}
---
+++ client/src/components/WriteArticle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WriteArticle(){
+    return(
+        <di>
+            Makale Yaz
+        </di>
+    )
+}
